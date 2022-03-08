6f7e5ffff53e3ebc070a8b553ad43a6eb5a1c5b1
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-08 16:37:40 +0000

    added JS and HTML

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
 
+
+class Model{
+    constructor(){
+        this.alerts = ["Do it again", "One more time", "NO TOUCHY!"]
+    }
+}
+
+class View{
+    constructor(){
+        this.buttonClicked = document.getElementById("button")//.addEventListener("click, showMsg")
+    }
+
+    bindButtonClick(buttonClicked){
+        this.button.addEventListener('click', showMsg)
+    }
+    
+}
+
+class Controller{
+    constructor(model, view){
+        this.m = model;
+        this.v = view;
+        this.v.bindButtonClick(this.buttonClicked)
+
+    }
+
+    async buttonClicked(e) {
+        
+    }
+    /*
+    function clickHandler() {
+        for(buttonClicked === true){
+            showMsg
+        }
+    }
+    */
+    function showMsg(alerts){
+        for (i=0; i<alerts.length; i++){
+            if([i] = !isNaN){
+                return alerts
+            }
+        }
+    }
+
+}
+
+const page = new Button(new Model(), new View())
